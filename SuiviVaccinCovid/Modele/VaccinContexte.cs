632a96afb0ee43cf0d73b54222b499ace0adaa61
RebeccaLapointe <reblapointe@gmail.com>
2021-02-28 19:17:19 -0500

    Refactoring : divisions en classes et en namespaces

---
+++ SuiviVaccinCovid/Modele/VaccinContexte.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuiviVaccinCovid.Modele
+{
+    public class VaccinContext : DbContext
+    {
+        public DbSet<Vaccin> Vaccins { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=VaccinBD;Trusted_Connection=True;");
+    }
+}
--- SuiviVaccinCovid/Program.cs
+++ SuiviVaccinCovid/Program.cs
@@ -1,87 +1,54 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using SuiviVaccinCovid.Modele;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace SuiviVaccinCovid
 {
-    public class Vaccin
-    {
-        public int VaccinId { get; set; }
-        public DateTime Date { get; set; }
-        public string NAMPatient { get; set; }
-        public string Type { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            return obj is Vaccin vaccin &&
-                   VaccinId == vaccin.VaccinId &&
-                   Date == vaccin.Date &&
-                   NAMPatient == vaccin.NAMPatient &&
-                   Type == vaccin.Type;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(VaccinId, Date, NAMPatient, Type);
-        }
-
-        public override string ToString()
-        {
-            return $" Vaccin #{VaccinId} ({Type}), adiminstré le {Date} à {NAMPatient}";
-        }
-    }
-
-    public class VaccinContext : DbContext
-    {
-        public DbSet<Vaccin> Vaccins { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=VaccinBD;Trusted_Connection=True;");
-    }
-
     public class Program
     {
+        public VaccinContext Contexte { get; set; }
+
         static void Main(string[] args)
         {
             Program p = new Program();
+            p.Contexte = new VaccinContext();
             p.Peupler();
 
-            VaccinContext context = new VaccinContext();
+            p.AjouterVaccin("LAPM12345678", "Pfizer");
 
-            p.AjouterVaccin(context, "LAPM12345678", "Pfizer");
-
-            Vaccin lePlusRecent = p.LePlusRecent(context.Vaccins);
+            Vaccin lePlusRecent = p.LePlusRecent();
             Console.WriteLine(lePlusRecent);
         }
 
         public void Peupler()
         {
-            VaccinContext context = new VaccinContext();
-
-            Vaccin dose1Mylene = new Vaccin
+            if (Contexte.Vaccins.Count() == 0)
             {
-                Date = new DateTime(2021, 01, 24),
-                NAMPatient = "LAPM12345678",
-                Type = "Moderna"
-            };
+                Vaccin dose1Mylene = new Vaccin
+                {
+                    Date = new DateTime(2021, 01, 24),
+                    NAMPatient = "LAPM12345678",
+                    Type = "Moderna"
+                };
 
-            Vaccin dose1Gaston = new Vaccin
-            {
-                Date = new DateTime(2021, 01, 15),
-                NAMPatient = "BHEG12345678",
-                Type = "Pfizer"
-            };
+                Vaccin dose1Gaston = new Vaccin
+                {
+                    Date = new DateTime(2021, 01, 15),
+                    NAMPatient = "BHEG12345678",
+                    Type = "Pfizer"
+                };
 
-            context.Vaccins.Add(dose1Mylene);
-            context.Vaccins.Add(dose1Gaston);
+                Contexte.Vaccins.Add(dose1Mylene);
+                Contexte.Vaccins.Add(dose1Gaston);
 
-            context.SaveChanges();
+                Contexte.SaveChanges();
+            }
         }
 
-        public void AjouterVaccin(VaccinContext contexte, string nam, string type)
+        public void AjouterVaccin(string nam, string type)
         {
-            var memePatient = contexte.Vaccins.Where(v => v.NAMPatient == nam);
+            var memePatient = Contexte.Vaccins.Where(v => v.NAMPatient == nam);
             if (memePatient.Count() > 1)
                 throw new ArgumentException("Patient déjà vacciné deux fois");
             if (memePatient.Count() == 1 && memePatient.First().Type != type)
@@ -93,13 +60,18 @@ namespace SuiviVaccinCovid
                 Type = type,
                 Date = DateTime.Now
             };
-            contexte.Add(v);
-            contexte.SaveChanges();
+            Contexte.Add(v);
+            Contexte.SaveChanges();
         }
 
         public Vaccin LePlusRecent(IEnumerable<Vaccin> vaccins)
         {
             return vaccins.OrderBy(v => v.Date).Last();
+        }
+
+        public Vaccin LePlusRecent()
+        {
+            return LePlusRecent(Contexte.Vaccins);
         }
     }
 }
